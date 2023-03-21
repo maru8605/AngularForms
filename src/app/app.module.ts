624d78b993eebd51172501040226a1ee852510e1
maru8605 <maru8605@hotmail.com>
2023-03-21 12:21:00 -0300

    chore: formularios reactivos validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
@@ -18,6 +18,7 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
+  forma: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
   }
 
+  ngOnInit(): void {}
+
+  get nombreNoValido() {
+    return (
+      this.forma.get('nombre').invalid &&
+      this.forma.get('nombre').untouched 
+      
+    );
+  }
+
+  get apellidoNoValido() {
+    return (
+      this.forma.get('apellido').invalid &&
+      this.forma.get('apellido').untouched 
+      
+    );
+  }
+  get correoNoValido() {
+    return (
+      this.forma.get('correo').invalid &&
+      this.forma.get('correo').untouched 
+      
+    );
+  }
+
+  crearFormulario() {
+    this.forma = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]], //segundo argumentos son los validadores sincronos
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      correo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
+    });
+  }
+
+  guardar() {
+    console.log(this.forma);
+    
+
+    // if (this.forma.invalid) {
+    //   Object.values(this.forma.controls).forEach((control) => {
+    //     control.markAsTouched();
+    //   });
+    // }
+  }
 }
